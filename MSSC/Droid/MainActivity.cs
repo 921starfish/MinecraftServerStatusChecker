3b1cc1ae6945c547f176ea89761069e9fe155113
921starfish <yuya.921.hoshino@gmail.com>
2016-02-06 16:18:57 +0900

    axmlをメインとした設計にできるように一部修正

--- MSSC/Droid/MainActivity.cs
+++ MSSC/Droid/MainActivity.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-
 using Android.App;
 using Android.Content;
-using Android.Content.PM;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
@@ -10,17 +8,24 @@ using Android.OS;
 
 namespace MSSC.Droid
 {
-	[Activity (Label = "MSSC.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
-	{
-		protected override void OnCreate (Bundle bundle)
-		{
-			base.OnCreate (bundle);
+    [Activity(Label = "MSSC.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
 
-			global::Xamarin.Forms.Forms.Init (this, bundle);
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
 
-			LoadApplication (new App ());
-		}
-	}
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+        }
+    }
 }
 
