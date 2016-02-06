bdbcdcaa64608b20ec5743f19f20fe2658e5eb9a
wang <azelf.trickroom@gmail.com>
2016-02-07 06:40:48 +0900

    データ保存用クラスを追加

---
+++ MSSC/iOS/MSSC/DataManager.swift
@@ -0,0 +1,44 @@
+//
+//  DataManager.swift
+//  MSSC
+//
+//  Created by 王一道 on 2016/02/07.
+//  Copyright © 2016年 王一道. All rights reserved.
+//
+
+import Foundation
+
+struct SaveObject{
+    internal var length: Int = 0
+    internal var serverArray: [(name:String, host:String, port:String)] = []
+}
+
+class DataManager{
+    
+    func save(data :SaveObject){
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        
+        userDefaults.setObject(data.length, forKey: "length")
+        
+        for(var i = 0; i < data.length; i++){
+            userDefaults.setObject(data.serverArray[i].name, forKey: "name" + String(i))
+            userDefaults.setObject(data.serverArray[i].host, forKey: "host" + String(i))
+            userDefaults.setObject(data.serverArray[i].port, forKey: "port" + String(i))
+        }
+    }
+    
+    func load()->SaveObject{
+        var data = SaveObject()
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        
+        data.length = userDefaults.integerForKey("length")
+        
+        for(var i = 0; i < data.length; i++){
+            let name = userDefaults.stringForKey("name" + String(i))
+            let host = userDefaults.stringForKey("host" + String(i))
+            let port = userDefaults.stringForKey("port" + String(i))
+            data.serverArray.append((name:name!,host:host!,port:port!))
+        }
+        return data
+    }
+}
